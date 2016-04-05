f3b76e838f9a40f045d45e66707f6a8a48d5ecdc
MTAZ <mtafro@gmail.com>
2016-04-05 14:08:29 -0700

    Update app.js
    Remove img animation

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 
-  var mouseX, mouseY;
-  var gameX = 100;
-  var gameY = 100;
+//  var mouseX, mouseY;
+//  var gameX = 100;
+//  var gameY = 100;
 
-  $(document).mousemove(function(e){
-    mouseX = e.clientX;
-    mouseY = e.clientY;
+//  $(document).mousemove(function(e){
+//   mouseX = e.clientX;
+//    mouseY = e.clientY;
 
-  }).mouseover();
+//  }).mouseover();
 
 
 
@@ -17,12 +17,12 @@ $(document).ready(function(){
               animBG();
           }, 25);
 
-        this.hero = setInterval(function () {
-        animImg();
-          }, 10);
+  //      this.hero = setInterval(function () {
+  //      animImg();
+  //        }, 10);
         }).bind('mouseleave', function () {
             this.bg && clearInterval(this.bg);
-            this.hero && clearInterval(this.hero);
+  //          this.hero && clearInterval(this.hero);
         });
 
 
@@ -32,11 +32,11 @@ $(document).ready(function(){
         $('#openings').css('background-position', position);
        }
 
-      function animImg() {
-        $('#hero').css('top', function(){
-          return mouseY;
-        });
-      }
+  //    function animImg() {
+  //      $('#hero').css('top', function(){
+  //        return mouseY;
+  //      });
+  //    }
 
 
 

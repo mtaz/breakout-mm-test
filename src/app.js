2c8829f8cbc17fc49546e24d43d89a5d0f363868
MTafro <mtafro@gmail.com>
2016-04-05 18:49:12 -0700

    update files

--- src/app.js
+++ src/app.js
@@ -1,14 +1,9 @@
 $(document).ready(function(){
 
-  var mouseX, mouseY;
-  var gameX = 100;
-  var gameY = 100;
 
-  $(document).mousemove(function(e){
-    mouseX = e.clientX;
-    mouseY = e.clientY;
+   var gameX = 100;
+   var gameY = 100;
 
-  }).mouseover();
 
 
 
@@ -16,13 +11,9 @@ $(document).ready(function(){
         this.bg = setInterval(function () {
               animBG();
           }, 25);
-
-        this.hero = setInterval(function () {
-        animImg();
-          }, 10);
         }).bind('mouseleave', function () {
             this.bg && clearInterval(this.bg);
-            this.hero && clearInterval(this.hero);
+
         });
 
 
@@ -31,13 +22,5 @@ $(document).ready(function(){
         var position = gameX + 'px ' + gameY + '%';
         $('#openings').css('background-position', position);
        }
-
-      function animImg() {
-        $('#hero').css('top', function(){
-          return mouseY;
-        });
-      }
-
-
 
 });
